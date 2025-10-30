5f5df45134dced10578dbfc7046ef82ba42b3daa
Pankaj Singh <75833262+Pankjsingh1028@users.noreply.github.com>
2025-10-30 15:11:04 +0530

    Modify WebSocket startup to prevent multiple initiations
    Refactor WebSocket startup to run in a thread only once.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
-# wsgi.py
 from f2fspread_main import app, start_streamer
 from datetime import datetime
+import threading
 
-print(f"[{datetime.now()}] Starting WebSocket...")
-start_streamer()
+def start_ws_once():
+    if not getattr(start_ws_once, "started", False):
+        print(f"[{datetime.now()}] Starting WebSocket thread...")
+        start_streamer()
+        start_ws_once.started = True
+
+start_ws_once()
 application = app.server
